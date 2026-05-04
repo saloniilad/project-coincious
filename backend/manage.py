c6643f5dc20369426190756116b28315546a5dc3
saloniilad <saloni.lad018@gmail.com>
2026-05-04 20:23:14 +0530

    fix: use production settings for deployment

--- backend/manage.py
+++ backend/manage.py
@@ -4,7 +4,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings_production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
